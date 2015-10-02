2ced1ea563517b6bd5b3d425e2b0a748286deab4
Alex Harris and Ryan Goldenberg <appacademy@Apps-Mac-mini-28.local>
2015-10-02 11:32:35 -0400

    Make Board class and populate method.

--- minesweeper.rb
+++ minesweeper.rb
@@ -17,3 +17,21 @@ class Square
   end
 
 end
+
+class Board
+  attr_reader :grid
+  DEFAULT_BOMB_PERCENT = 0.3
+
+  def initialize()
+    @grid = []
+  end
+
+  def populate(board_size = 9, bomb_percent = DEFAULT_BOMB_PERCENT)
+    @grid = (0...board_size).map do |row|
+      (0...board_size).map do |col|
+        rand < bomb_percent ? Square.new(true) : Square.new(false)
+      end
+    end
+
+  end
+end
