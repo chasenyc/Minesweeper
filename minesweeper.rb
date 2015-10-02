83e3df3297a0d329e10991a598563438b5149aa5
Alex Harris and Ryan Goldenberg <appacademy@Apps-Mac-mini-28.local>
2015-10-02 12:29:09 -0400

    Add game class and difficulties.

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,5 +1,6 @@
 class Square
   attr_reader :bomb, :hidden, :flagged, :value
+  alias_method :bomb?, :bomb
 
   def initialize(bomb = false)
     @bomb = bomb
@@ -28,6 +29,7 @@ class Square
     @value = value
   end
 
+
 end
 
 class Board
@@ -35,8 +37,9 @@ class Board
   DEFAULT_BOMB_PERCENT = 0.3
   OFFSETS = (-1..1).to_a.repeated_permutation(2).to_a.delete_if { |a| a == [0, 0] }
 
-  def initialize()
+  def initialize(board_size = 9, bomb_percent = DEFAULT_BOMB_PERCENT)
     @grid = []
+    populate(board_size, bomb_percent)
   end
 
   def [](pos)
@@ -49,24 +52,36 @@ class Board
     @grid[x][y] = value
   end
 
-  def populate(board_size = 9, bomb_percent = DEFAULT_BOMB_PERCENT)
+  def populate(board_size, bomb_percent)
     @grid = (0...board_size).map do |row|
       (0...board_size).map do |col|
         rand < bomb_percent ? Square.new(true) : Square.new(false)
       end
     end
+    set_values
   end
 
   def set_values
     (0...grid.length).each do |row|
       (0...grid.length).each do |col|
-        num_bombs = get_neighbors([])
+        pos = [row, col]
+        if self[pos].bomb?
+          self[pos].set_value("B")
+        else
+          self[pos].set_value(neighbor_bomb_count(pos))
+        end
       end
     end
   end
 
+  def neighbor_bomb_count(pos)
+    get_neighbors(pos).count(&:bomb?)
+  end
+
   def get_neighbors(pos)
-    OFFSETS.map { |offset| add_squares(offset, pos) }.select { |square| valid_pos?(square) }
+    OFFSETS.map do |offset|
+      add_squares(offset, pos)
+    end.select { |square| valid_pos?(square) }.map { |pos| self[pos] }
   end
 
   def add_squares(square1, square2)
@@ -78,9 +93,24 @@ class Board
   end
 
   def to_s
-    p grid.map do |row|
+    grid.map do |row|
       row.map(&:to_s).join(" ")
     end.join("\n")
   end
 
+  def render
+    system("clear")
+    puts to_s
+  end
+
+end
+
+class Game
+  attr_reader :board
+  DIFFICULTIES = (1..5).reduce({}) { |accum,level| accum.merge({level => (level * 0.1).round(2) }) }
+
+  def initialize(board_size = 9, difficulty = 5)
+    @board = Board.new(board_size, DIFFICULTIES[difficulty])
+  end
+  
 end
